e9675dee5e6522b40336734fd4756b6b8588aa06
dzupin <dzupinr@gmail.com>
2016-06-30 16:36:06 +0200

    few more libraries

--- uncategorized.py
+++ uncategorized.py
@@ -13,3 +13,32 @@ print("IPython version: %s" % IPython.__version__)
 import sklearn
 print("scikit-learn version: %s" % sklearn.__version__)
 
+import pygame
+print("pygame version: %s" % pygame.__version__)
+
+############################################################################################
+import  requests_cache
+import requests
+
+import dataset
+db = dataset.connect('sqlite:///data_wrangling.db')
+my_data_source = {
+    'url':
+    'http://www.tsmplug.com/football/premier-league-player-salaries-club-by-club/',
+    'description': 'Premier League Club Salaries',
+    'topic': 'football',
+    'verified': False,
+}
+table = db['data_sources']
+table.insert(my_data_source)
+another_data_source = {
+    'url':
+    'http://www.premierleague.com/content/premierleague/en-gb/players/index.html',
+    'description': 'Premier League Stats',
+    'topic': 'football',
+    'verified': True,
+}
+table.insert(another_data_source)
+sources = db['data_sources'].all()
+print sources
+print sources.row_type
