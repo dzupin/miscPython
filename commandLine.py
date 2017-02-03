3e19d377b271c79070ae63275e4b7b77b61888c8
dzupin <dzupinr@gmail.com>
2017-02-03 15:44:13 +0100

    use of subprocess call

--- commandLine.py
+++ commandLine.py
@@ -1,6 +1,27 @@
-import subprocess32 as subprocess
+#import subprocess32 as subprocess
+#import os
+
+import subprocess
+subprocess.call("ls -al >> mylogfile.log", shell=True)
+subprocess.call("date >> mylogfile.log", shell=True)
+subprocess.call("pwd >> mylogfile.log", shell=True)
+
+proc = subprocess.Popen(['tail', '-500', '/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
+for line in proc.stdout.readlines():
+    print line.rstrip()
+
+
 #if os.name == 'posix' and sys.version_info[0] < 3:
 #    import subprocess32 as subprocess
 #else:
 #    import subprocess
-print subprocess.check_output(['dir','/Q'])
+#print subprocess.check_output(['ls','-l'])
+#print subprocess.check_output(['pwd'])
+
+#cmd = 'whoami;date;pwd;'
+#os.system(cmd)
+
+#cmd2 = 'whoami;date;pwd'
+#os.system(cmd2)
+
+#os.system('whoami;date;pwd;date;date;date')
