dcc4ffae17ce646e8330d0c92b0b05316e83911a
dzupin <dzupinr@gmail.com>
2017-02-03 16:14:46 +0100

    use of subprocess call

--- commandLine.py
+++ commandLine.py
@@ -2,17 +2,21 @@
 #import os
 
 import subprocess
-subprocess.call("ls -al >> mylogfile.log", shell=True)
-subprocess.call("date >> mylogfile.log", shell=True)
-subprocess.call("pwd >> mylogfile.log", shell=True)
+#subprocess.call("ls -al >> mylogfile.log", shell=True)
+#subprocess.call("date >> mylogfile.log", shell=True)
+#subprocess.call("pwd >> mylogfile.log", shell=True)
 
 
-subprocess.run("pwd")
-subprocess.run(["ls", "-l"])  # doesn't capture output
+
+
+subprocess.call("cd;pwd", shell=True)
+#subprocess.call(["pwd;cd /TEMP;pwd"], shell=True)
+subprocess.call("pwd")
+subprocess.call(["ls", "-l"])  # doesn't capture output
 
 
 #proc = subprocess.Popen(['tail', '-500', '/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
-proc = subprocess.Popen(['tail', '/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
+proc = subprocess.Popen(['tail', '-2','/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
 for line in proc.stdout.readlines():
     print (line.rstrip())
 
