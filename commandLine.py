99fd8e93f19e8cfa99a171b669efa012e44c1e10
dzupin <dzupinr@gmail.com>
2017-02-03 17:39:21 +0100

    use of subprocess call

--- commandLine.py
+++ commandLine.py
@@ -6,17 +6,21 @@ import subprocess
 #subprocess.call("date >> mylogfile.log", shell=True)
 #subprocess.call("pwd >> mylogfile.log", shell=True)
 
+##### initialize Aion ###############
+cmdTestRoutine="cd /opt/CA/AionBRE;. ./aion.sh;cd /opt/CA/AionBRE/examples/Associate; rm /TEMP/Out.log; rm -rf /opt/CA/AionBRE/examples/Associate/associate.bin; respawn associate2.app >> /TEMP/Out.log  2>&1;reexec associate >> /TEMP/Out.log 2>&1 ;cd associate.bin;./_associate >> /TEMP/Out.log 2>&1 "
+subprocess.call(cmdTestRoutine,shell=True)
+#subprocess.call("cd /opt/CA/AionBRE;pwd;ls -la;. ./aion.sh;cd /opt/CA/AionBRE/examples/Associate;respawn associate.app;reexec associate;pwd;date;",shell=True)
 
 
 
-subprocess.call("cd;pwd", shell=True)
+#subprocess.call("cd;pwd", shell=True)
 #subprocess.call(["pwd;cd /TEMP;pwd"], shell=True)
-subprocess.call("pwd")
-subprocess.call(["ls", "-l"])  # doesn't capture output
+#subprocess.call("pwd")
+#subprocess.call(["ls", "-l"])  # doesn't capture output
 
 
 #proc = subprocess.Popen(['tail', '-500', '/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
-proc = subprocess.Popen(['tail', '-2','/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
+proc = subprocess.Popen(['tail', '-2000','/TEMP/Out.log'], stdout=subprocess.PIPE)
 for line in proc.stdout.readlines():
     print (line.rstrip())
 
