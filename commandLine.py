87e00f0fbed8d52396c252f7402425f65bd2de41
dzupin <dzupinr@gmail.com>
2017-02-03 18:49:34 +0100

    use of subprocess call

--- commandLine.py
+++ commandLine.py
@@ -1,13 +1,5 @@
-#import subprocess32 as subprocess
-#import os
-
 import subprocess
-#subprocess.call("ls -al >> mylogfile.log", shell=True)
-#subprocess.call("date >> mylogfile.log", shell=True)
-#subprocess.call("pwd >> mylogfile.log", shell=True)
-
-##### initialize Aion ###############
-#cmdTestRoutine="cd /opt/CA/AionBRE;. ./aion.sh;cd /opt/CA/AionBRE/examples/Associate; rm /TEMP/Out.log; rm -rf /opt/CA/AionBRE/examples/Associate/associate.bin; respawn associate2.app >> /TEMP/Out.log  2>&1;reexec associate >> /TEMP/Out.log 2>&1 ;cd associate.bin;./_associate >> /TEMP/Out.log 2>&1 "
+####import subprocess32 as subprocess
 
 # Setup your execution command
 goToInstallDir="cd /opt/CA/AionBRE;"
@@ -18,29 +10,21 @@ respawnApp= "  respawn associate.app >> /TEMP/Out.log  2>&1;"
 reexecRun= "reexec associate >> /TEMP/Out.log 2>&1 ;"
 binaryRun= "cd associate.bin;./_associate >> /TEMP/Out.log 2>&1 "
 
+#Put together execution command
 cmdTestRoutine=goToInstallDir + initializeEnvironment + goToTestExample + deleteOldLogsAndTestFiles + respawnApp + reexecRun + binaryRun
 
-#cmdTestRoutine="cd /opt/CA/AionBRE;. ./aion.sh;cd /opt/CA/AionBRE/examples/Associate; rm /TEMP/Out.log; rm -rf /opt/CA/AionBRE/examples/Associate/associate.bin; respawn associate2.app >> /TEMP/Out.log  2>&1;reexec associate >> /TEMP/Out.log 2>&1 ;cd associate.bin;./_associate >> /TEMP/Out.log 2>&1 "
-
-
-
+#Run econcatenated execution commnand
 subprocess.call(cmdTestRoutine,shell=True)
-#subprocess.call("cd /opt/CA/AionBRE;pwd;ls -la;. ./aion.sh;cd /opt/CA/AionBRE/examples/Associate;respawn associate.app;reexec associate;pwd;date;",shell=True)
 
-
-
-#subprocess.call("cd;pwd", shell=True)
-#subprocess.call(["pwd;cd /TEMP;pwd"], shell=True)
-#subprocess.call("pwd")
-#subprocess.call(["ls", "-l"])  # doesn't capture output
-
-
-#proc = subprocess.Popen(['tail', '-500', '/TEMP/mylogfile.log'], stdout=subprocess.PIPE)
+#Example how to open and parse log file using shell command tail on Linux (If possible, use Python build-in file processing instead)
 proc = subprocess.Popen(['tail', '-2000','/TEMP/Out.log'], stdout=subprocess.PIPE)
 for line in proc.stdout.readlines():
     print (line.rstrip())
 
 
+
+#Scratchpad AREA
+#  import os
 #if os.name == 'posix' and sys.version_info[0] < 3:
 #    import subprocess32 as subprocess
 #else:
