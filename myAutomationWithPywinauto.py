118acbbbbb2509717a386d97b35c1884a1f249f2
dzuro02 <Robert.Dzupin@ca.com>
2017-02-15 17:48:47 +0100

    Adding some control loops while processing output

--- myAutomationWithPywinauto.py
+++ myAutomationWithPywinauto.py
@@ -1,5 +1,8 @@
+from __future__ import print_function
 from pywinauto.application import Application
 import subprocess
+import sys
+import time
 #NOTE: Python 2.7.13 fails import of pywinauto (http://pywinauto.github.io/)
 #library: Issue29294  ctypes.windll.LoadLibrary refuses unicode argument
 #Therefore use 2.7.12 (or Python 3) or wait for 2.7.14 where it will be fixed ( http://bugs.python.org/issue29294)
@@ -24,9 +27,33 @@ file = open('C:\TEMP\examples\Associate\Out.log', 'r')
 print (file.read())
 
 #from pywinauto.application import Desktop
-appAion = Application().start(r"C:\TEMP\examples\Associate\associate.bin\_associate.exe")
+try:
+    #appAion = Application().start(r"C:\TEMP\examples\Associate\associate.bin\_associate.exe")
+    appAion = Application().start(r"reexec C:\TEMP\examples\Associate\associate.app")
+except Exception:
+    print ("Generic exception: Application failed to start")
+except:
+    print ("something else went wrong")
+    sys.exit()
 #appAion.print_control_identifiers()
 print("Second round")
+
+
+#Check for specific Dialog box window that in this case indicate missing test file
+if appAion.window(title = "Aion Init").Exists():
+    print ("Aion script that you are referencing does not exist.")
+    print(appAion['Dialog'].Texts())
+    print(appAion['Dialog']['Static'].GetProperties())
+    print(appAion['Dialog']['Static'].Texts())
+    print(appAion['Dialog']['Button'].Texts())
+    print(appAion['Dialog'].capture_as_image().save("screen_capture_InitError.png", "PNG"))
+    appAion['Dialog']['Button'].click()
+    print("Because test app is not available we should exit the test")
+    sys.exit()
+
+
+
+
 dlg = appAion['Dialog']
 dlg.print_control_identifiers()
 appAion['Dialog']['Button'].print_control_identifiers()
@@ -50,23 +77,36 @@ print ("++++")
 #print appAion[u'Title']['OK'].debug_message("My debug 2")   # writes debug message over control (see in saved PIL image) <pywinauto.controls.win32_controls.ButtonWrapper object at 0x0000000006F0AC88>
 #print appAion[u'Title']['OK'].capture_as_image().save("screen_capture2.jpg", "JPEG")  #<PIL.Image.Image image mode=RGB size=100x30 at 0x6E722E8>
 print ("-------------------- **** -----------------------")
-print (appAion['Dialog']['Static'].Texts())
+if appAion['Dialog'].Exists():
+    print (appAion['Dialog']['Static'].Texts())
 appAion['Dialog']['Button'].click()
-print (appAion['Dialog']['Static'].Texts())
+if appAion['Dialog'].Exists():
+    print (appAion['Dialog']['Static'].Texts())
 appAion[u'dialog']['OK'].click()
-print (appAion['Title']['Static'].Texts())
+if appAion['Dialog'].Exists():
+    print (appAion['Title']['Static'].Texts())
 appAion[u'Title']['OK'].click()
-print (appAion['Dialog']['Static'].Texts())
+if appAion['Dialog'].Exists():
+    print (appAion['Dialog']['Static'].Texts())
+if appAion['Dialog'].Exists():
+    appAion['Dialog']['Button'].click()
+
+if appAion['Dialog'].Exists():
+    print(appAion['Dialog']['Static'].Texts())
+else:
+    sys.exit()
+
+print ("Continue .... ")
 appAion['Dialog']['Button'].click()
-print (appAion[u'Title'][u'Static'].Texts())
-appAion['Dialog']['Button'].click()
-print (appAion['Dialog']['Static'].Texts())
-appAion['Dialog']['Button'].click()
-print (appAion['Dialog']['Static'].Texts())
-appAion['Dialog']['Button'].click()
-print (appAion['Dialog']['Static'].Texts())
-appAion['Dialog']['Button'].debug_message("My debug")
-appAion['Dialog']['Button'].click()
+
+while appAion['Dialog'].Exists():
+    print ("inside of while loop")
+    print(appAion['Dialog']['Static'].Texts())
+    appAion['Dialog']['Button'].click()
+else:
+    print ("All is done ...")
+    sys.exit()
+
 
 
 print ("done with test")
