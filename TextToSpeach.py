477debebcd8ed478a57accf5ef98d30c5132f80f
dzupin <dzupin@gmail.com>
2018-05-09 11:08:59 +0200

    Added replay of saved mp3 speech outputs.

--- TextToSpeach.py
+++ TextToSpeach.py
@@ -3,8 +3,19 @@ from gtts import gTTS
 
 tts = gTTS(text='The quick brown fox jumped over the lazy dog.', lang='en')
 tts.save('Audiofile.mp3')
-tts = gTTS(text="Pyttsx is a good text to speech conversion library in python but it was written only in python2 untill now ! Even some fair amount of googling didn't help much to get a tts library compatible with Python3. There is however , one library gTTS which works perfectly in python3 but it needs internet connection to work since it relies on google to get the audio data.But Pyttsx is completely offline and works seemlesly and has multiple tts-engine support.The codes in this repos are modified version of the pyttsx module of python 2.x and most of it is implemented from westonpace's repo. The purpose of creating this repo is to help those who want to have an offline tts lib for Python3 and don't want to port it from python2 to python3 themselves.", lang='en')
+tts = gTTS(text="Pyttsx is a good text to speech conversion library in python but it was written only in python2. Untill now!", lang='en')
 tts.save('Audiofile2.mp3')
+
+#Now lets play saved audio mp3 files
+import time
+from pygame import mixer # Load the required library
+mixer.init()
+mixer.music.load('Audiofile.mp3')
+mixer.music.play()
+time.sleep(6)
+mixer.music.load('Audiofile2.mp3')
+mixer.music.play()
+time.sleep(12)
 
 #Offline text to speach
 import pyttsx3
